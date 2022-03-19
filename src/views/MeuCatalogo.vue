2ec533ad07e0905b1a62aed89f0f3d892ca0ab55
Wesley Gomes Lima Melo <wesley.gomeslm@gmail.com>
2022-03-19 15:49:05 -0300

    Meu catalogo teste

--- src/views/MeuCatalogo.vue
+++ src/views/MeuCatalogo.vue
@@ -1,10 +1,35 @@
 <template>
-    <ul> 
-        <li v-for="item of catalogoLista" :key="item.id">
-                <img :src="item.imagem">
-        </li>
-    </ul>
-  
+    <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined 
+     >
+        <v-list-item three-line 
+        v-for="item of catalogoLista" :key="item.id">
+        <v-list-item-avatar
+            tile
+            size="80"
+            color="grey"
+        >
+            <v-img :src="item.imagem"></v-img>
+        </v-list-item-avatar>
+
+        <div class="text mb-4">
+            {{ item.nome }}
+        </div>
+        
+        <v-card-actions>
+            <v-btn
+                outlined
+                rounded
+                text
+            >
+                Adicionar
+            </v-btn>
+        </v-card-actions>
+        <v-divider></v-divider>
+        </v-list-item>
+  </v-card>
 </template>
 
 <script>
@@ -16,10 +41,10 @@
         }
         },
         created() {
-        fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
-            .then(response => response.json())
-            .then(json => {
-            this.catalogoLista = json;
+            fetch('https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json')
+                .then(response => response.json())
+                .then(json => {
+                this.catalogoLista = json;
             })
         }
     }
