8c9debc51183c0526f84cd737654882e3e571523
Leonardo Lucas <leonardolucas2020@gmail.com>
2022-03-19 11:11:12 -0400

    Criando e Estilizando a Tela de Pedido

---
+++ src/views/MeuPedido.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="meu-pedido">
+    <v-card class="mx-auto my-12" max-width="374">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+
+      <v-img
+        height="250"
+        src="https://t2.rg.ltmcdn.com/pt/posts/8/5/2/ovo_de_pascoa_prestigio_9258_orig.jpg"
+      ></v-img>
+
+      <v-card-title>Resumo do Pedido</v-card-title>
+
+      <v-card-text>
+        <div class="my-1">
+          <span class="title">Sabor: </span
+          ><span class="subtitle-1">Recheado Com Mousse de Morango</span>
+        </div>
+        <v-divider></v-divider>
+        <div class="my-1">
+          <span class="title">Quantidade: </span>
+          <span class="subtitle-1">3</span>
+        </div>
+
+        <v-divider></v-divider>
+        <div class="my-1">
+          <span class="title">Preço Unitario: </span>
+          <span class="subtitle-1">R$ 26,00</span>
+        </div>
+        <v-divider></v-divider>
+        <div class="my-1">
+          <span class="title">Total do Pedido: </span>
+          <span class="subtitle-1">R$ 78,00</span>
+        </div>
+        <v-divider></v-divider>
+        <div class="my-1">
+          <span class="title">Endreço de Entrega: </span>
+          <span class="subtitle-1"
+            >Rua Taighum LLL, 566 - Paisagem Loud - Manaus - AM</span
+          >
+        </div>
+      </v-card-text>
+
+      <v-divider class="mx-4"></v-divider>
+
+      <v-card-title>Previsão de Entrega</v-card-title>
+
+      <v-card-text>
+        <span class="subtitle-1"> O Seu Pedido chegará em até 5 Horas. </span>
+      </v-card-text>
+
+      <v-card-actions class="justify-space-between">
+        <v-btn color="success"> Confirmar Pedido </v-btn>
+        <v-btn color="error"> Cancelar Pedido </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MeuPedido",
+};
+</script>
+
+<style>
+.verde {
+  background-color: #66bb6a;
+}
+</style>
