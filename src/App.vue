e9c3292754f537b410bcfc8ac4722848d23b821e
carolinelds <carolinelds@gmail.com>
2022-03-19 10:32:14 -0300

    Adiciona estrutura vue com Router e Vuetify

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <template>
+      <v-app>
+        <v-content>
+          <router-view></router-view>
+        </v-content>
+      </v-app> </template
+  ></v-app>
+</template> 
+
+ 
+
+<script> 
+
+export default { 
+
+  name: 'App' 
+
+  } 
+
+}; 
+
+</script> 
+
+ 
+<style>
+</style>
+  
